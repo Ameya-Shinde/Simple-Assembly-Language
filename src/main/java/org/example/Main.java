ec047c89019d00184c10857284d4a10438fff543
gr1mw1k <ameyashinde3@gmail.com>
2023-05-10 13:09:56 +0530

    Optimized Command Inputs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,24 +8,30 @@ public class Main {
 
         CreateREG createREG = new CreateREG();
         createREG.loadHashMap();
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your command \n (Use ';' when giving multiple commands) : ");
-        String input = sc.nextLine();
-        String actions[] = input.split(";");
-        for(String action: actions) {
-            if(action.charAt(0) == 'M'){
-                createREG.createREG(action);
-            }else if(action.charAt(0) == 'A'){
-                AddToREG addToREG = new AddToREG();
-                addToREG.command(action);
-            }else if(action.charAt(0) == 'S'){
-                ShowREG showREG = new ShowREG();
-                showREG.showResult();
-            }else{
-                System.out.println("Unknown Command");
+        boolean flag = true;
+        System.out.println("\nEnter your command (Use ';' when giving multiple commands) \nEnter 'EXIT' when you are done with your operations");
+        while(flag){
+            System.out.println();
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            String actions[] = input.split(";");
+            for(String action: actions) {
+                if(action.charAt(0) == 'M'){
+                    createREG.createREG(action);
+                }else if(action.charAt(0) == 'A'){
+                    AddToREG addToREG = new AddToREG();
+                    addToREG.command(action);
+                }else if(action.charAt(0) == 'S'){
+                    ShowREG showREG = new ShowREG();
+                    showREG.showResult();
+                }else if(action.charAt(0) == 'E'){
+                    flag = false;
+                }else{
+                    System.out.println("Unknown Command");
+                }
             }
         }
+
         createREG.storeHashMap();
     }
 }
